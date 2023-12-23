4e82d8b309063b8f73f4ee0ff90cbe5dc0890b30
SahilNalavade <sahilnalavade25@gmail.com>
2023-12-23 13:32:26 +0530

    chakra added to songForm

--- components/SongForm.tsx
+++ components/SongForm.tsx
@@ -7,6 +7,14 @@ import { firestore } from '../firebase'; // Adjust the path accordingly
 import { getAuth } from 'firebase/auth';
 import { auth } from '../firebase'; // Adjust the path accordingly
 import { onAuthStateChanged } from 'firebase/auth';
+import {
+  Input,
+  Button,
+  List,
+  ListItem,
+  VStack,
+  Box,
+} from '@chakra-ui/react';
 
 interface SongFormProps {
   onSubmit: (songs: string[], username: string) => void;
@@ -21,12 +29,16 @@ const SongForm: React.FC<SongFormProps> = ({ onSubmit }) => {
   const debouncedSearch = _.debounce(async (input: string) => {
     try {
       // Make a request to the Last.fm API
-      const response = await axios.get(`https://ws.audioscrobbler.com/2.0/?method=track.search&track=${input}&api_key=91e0b0d00eefb1d4b39d5a6b44f1ba75&format=json`);
-      
+      const response = await axios.get(
+        `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${input}&api_key=91e0b0d00eefb1d4b39d5a6b44f1ba75&format=json`
+      );
+
       // Check if the necessary properties are present in the response
       if (response.data.results && response.data.results.trackmatches) {
         // Extract the song names from the response and update the state
-        const songs = response.data.results.trackmatches.track.map((item: any) => item.name);
+        const songs = response.data.results.trackmatches.track.map(
+          (item: any) => item.name
+        );
         setSearchResults(songs);
       } else {
         // Handle the case where the expected properties are not present
@@ -37,13 +49,17 @@ const SongForm: React.FC<SongFormProps> = ({ onSubmit }) => {
     }
   }, 300);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     const input = event.target.value;
     setSongInput(input);
     debouncedSearch(input);
   };
 
-  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleUsernameChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     const input = event.target.value;
     setUsernameInput(input);
   };
@@ -87,21 +103,45 @@ const SongForm: React.FC<SongFormProps> = ({ onSubmit }) => {
   };
 
   return (
-    <div>
-      <input type="text" placeholder="Enter username" value={usernameInput} onChange={handleUsernameChange} />
-      <input type="text" placeholder="Enter song" value={songInput} onChange={handleInputChange} />
-      
-      <h2>Search Results</h2>
-      <ul>
-        {searchResults.map((result, index) => (
-          <li key={index} onClick={() => handleSelectSong(result)}>
-            {result}
-          </li>
-        ))}
-      </ul>
+    <VStack spacing={4} align="center">
+      <Input
+        type="text"
+        placeholder="Enter username"
+        value={usernameInput}
+        onChange={handleUsernameChange}
+        variant="filled"
+        mb={2}
+      />
+      <Input
+        type="text"
+        placeholder="Enter song"
+        value={songInput}
+        onChange={handleInputChange}
+        variant="filled"
+        mb={2}
+      />
 
-      <button onClick={handleSubmit}>Submit</button>
-    </div>
+      <Box w="100%" bg="gray.100" p={4} borderRadius="md">
+        <h2>Search Results</h2>
+        <List>
+          {searchResults.map((result, index) => (
+            <ListItem
+              key={index}
+              onClick={() => handleSelectSong(result)}
+              cursor="pointer"
+              p={2}
+              _hover={{ bg: 'gray.200' }}
+            >
+              {result}
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+
+      <Button colorScheme="blue" onClick={handleSubmit}>
+        Submit
+      </Button>
+    </VStack>
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// _app.tsx
+import { ChakraProvider } from '@chakra-ui/react';
+ // Adjust the path accordingly
+import '@/styles/globals.css'; // Import your global styles
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+
+export default MyApp;
